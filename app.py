61970f32e50f968e8908e52b1e2179a4864bd843
Mehmet Batuhan Duman <58864224+bthndmn12@users.noreply.github.com>
2023-05-08 00:59:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -215,6 +215,6 @@ def gpreport():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8001)))
+    app.run(debug=False)
     #app.run(debug=True, port=8001)
 
